0ad123097bc001d36b8ca5bea4c191da3aeb53cc
tomaszferens <tomaszferens@hotmail.com>
2017-10-21 11:53:07 +0200

    new form component

---
+++ src/app/components/messageform/messageform.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-messageform",
+  template: `
+    <form>
+      <input />
+    </form>
+  `,
+  styleUrls: ["./messageform.component.css"]
+})
+export class MessageformComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
