cda5e114f8d956773d92a36160db79b19c262eb2
tomaszferens <tomaszferens@hotmail.com>
2017-10-21 12:09:14 +0200

    sending messages

--- src/app/components/messageform/messageform.component.ts
+++ src/app/components/messageform/messageform.component.ts
@@ -1,17 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AngularFireDatabase } from "angularfire2/database";
 
 @Component({
   selector: "app-messageform",
   template: `
-    <form>
-      from: <input />
-      message: <textarea></textarea>
+    <form [formGroup]="form" (ngSubmit)="submitForm()">
+    from: <input [formControlName]="'from'" />
+    message: <textarea  [formControlName]="'text'"></textarea>
+    <div>
+      <button type="submit" [disabled]="form.invalid">SEND</button>
+    </div>
+
     </form>
   `,
   styleUrls: ["./messageform.component.css"]
 })
-export class MessageformComponent implements OnInit {
-  constructor() {}
+export class MessageformComponent {
+  public form: FormGroup;
+  constructor(
+    public db: AngularFireDatabase,
+    private formBuilder: FormBuilder
+  ) {
+    this.form = formBuilder.group({
+      from: ["", Validators.required],
+      text: ["", Validators.required]
+    });
+  }
 
   ngOnInit() {}
+
+  submitForm() {
+    this.db.list("messages").push(this.form.value);
+  }
 }
